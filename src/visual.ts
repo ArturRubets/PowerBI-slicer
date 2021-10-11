46d6e28d4bb1950c5e3753212831d69aaa461b7c
Artur Rubets <artur.rubets@gmail.com>
2021-10-11 18:12:03 +0300

    Добавление свойств

--- src/visual.ts
+++ src/visual.ts
@@ -1,28 +1,3 @@
-/*
-*  Power BI Visual CLI
-*
-*  Copyright (c) Microsoft Corporation
-*  All rights reserved.
-*  MIT License
-*
-*  Permission is hereby granted, free of charge, to any person obtaining a copy
-*  of this software and associated documentation files (the ""Software""), to deal
-*  in the Software without restriction, including without limitation the rights
-*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-*  copies of the Software, and to permit persons to whom the Software is
-*  furnished to do so, subject to the following conditions:
-*
-*  The above copyright notice and this permission notice shall be included in
-*  all copies or substantial portions of the Software.
-*
-*  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-*  THE SOFTWARE.
-*/
 "use strict";
 
 import "core-js/stable";
@@ -35,15 +10,108 @@ import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInst
 import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
-
+import PrimitiveValue = powerbi.PrimitiveValue;
 import { VisualSettings } from "./settings";
+import { dataViewObjects } from "powerbi-visuals-utils-dataviewutils";
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
+
+interface Settings {
+    general: {
+        fill: string;
+        fontSize: number;
+    };
+    title: {
+        hide: boolean;
+        fontSizeTitle: number;
+        text: string;
+    }
+}
+
+interface Data {
+    data: PrimitiveValue[];
+}
+
+interface ViewModel {
+    dataModel: Data;
+    settings: Settings;
+}
+
+let defaultSettings: Settings = {
+    general: {
+        fill: '#4762D3',
+        fontSize: 15
+    },
+    title: {
+        fontSizeTitle: 15,
+        hide: false,
+        text: null
+    }
+};
+
+function visualTransform(options: VisualUpdateOptions): ViewModel {
+    let dataViews = options.dataViews;
+    let viewModel: ViewModel = {
+        dataModel: null,
+        settings: <Settings>{}
+    };
+
+    if (!dataViews
+        || !dataViews[0]
+        || !dataViews[0].categorical
+        || !dataViews[0].categorical.categories
+        || !dataViews[0].categorical.categories[0].source
+        || !dataViews[0].categorical.categories[0].values
+    ) {
+        return viewModel;
+    }
+
+    let categorical = dataViews[0].categorical;
+
+    let categories = categorical.categories[0]
+
+    let objects = dataViews[0].metadata.objects;
+
+
+    let settings: Settings = {
+        general: {
+            fill: dataViewObjects.getFillColor(objects, { objectName: 'general', propertyName: 'fill' }, defaultSettings.general.fill),
+            fontSize: dataViewObjects.getValue(objects, { objectName: "general", propertyName: "fontSize",
+            }, defaultSettings.general.fontSize),
+        },
+        title:{
+            text: dataViewObjects.getValue(objects, { objectName: "general", propertyName: "text"}, categories.source.displayName),
+            fontSizeTitle: dataViewObjects.getValue(objects, { objectName: "general", propertyName: "fontSizeTitle"}, defaultSettings.title.fontSizeTitle),
+            hide: dataViewObjects.getValue(objects, { objectName: "general", propertyName: "hide"}, defaultSettings.title.hide),
+        }
+    };
+
+
+    let data: Data = { data: categories.values};
+    
+    return {
+        dataModel: data,
+        settings: settings
+    };
+}
+
+
+
+
 export class Visual implements IVisual {
     private target: HTMLElement;
     private updateCount: number;
-    private settings: VisualSettings;
+    private settings: Settings;
     private textNode: Text;
+    
+    
+    private dataModel: Data;
+    private host: IVisualHost;
 
     constructor(options: VisualConstructorOptions) {
+        this.host = options.host;
+
+
+
         console.log('Visual constructor', options);
         this.target = options.element;
         this.updateCount = 0;
@@ -59,23 +127,78 @@ export class Visual implements IVisual {
     }
 
     public update(options: VisualUpdateOptions) {
-        this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
+        let viewModel: ViewModel = visualTransform(options);
+        this.settings = viewModel.settings;
+        this.dataModel = viewModel.dataModel;
+        console.log(viewModel);
+        
+        
+        //this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
         console.log('Visual update', options);
         if (this.textNode) {
             this.textNode.textContent = (this.updateCount++).toString();
         }
+
+        const div: HTMLElement = document.createElement("div");
+        div.style.width = '20px';
+        div.style.height = '20px';
+        div.style.backgroundColor = '#CCC';
+        div.innerText = "DIV"
+        this.target.appendChild(div)
+
+
     }
 
-    private static parseSettings(dataView: DataView): VisualSettings {
-        return <VisualSettings>VisualSettings.parse(dataView);
+    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): powerbi.VisualObjectInstanceEnumeration {
+        let objectName = options.objectName;
+        let objectEnumeration: VisualObjectInstance[] = [];
+
+        if (!this.settings ||
+            !this.settings.general ||
+            !this.settings.title) {
+            return objectEnumeration;
+        }
+
+        switch (objectName) {
+            case 'title':
+                objectEnumeration.push({
+                    objectName: objectName,
+                    properties: {
+                        hide: this.settings.title.hide,
+                        fontSizeTitle: this.settings.title.fontSizeTitle,
+                        text: this.settings.title.text
+                    },
+                    validValues: {
+                        fontSizeTitle: {
+                            numberRange: {
+                                min: 6,
+                                max: 40
+                            }
+                        }
+                    },
+                    selector: null
+                });
+                break;
+            case 'general':
+                objectEnumeration.push({
+                    objectName: objectName,
+                    properties: {
+                        fill: this.settings.general.fill,
+                        fontSize: this.settings.general.fontSize,
+                    },
+                    validValues: {
+                        fontSizeValue: {
+                            numberRange: {
+                                min: 0,
+                                max: 40
+                            }
+                        }
+                    },
+                    selector: null
+                });
+                break;
+        };
+        return objectEnumeration;
     }
 
-    /**
-     * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the
-     * objects and properties you want to expose to the users in the property pane.
-     *
-     */
-    public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-        return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
-    }
 }
